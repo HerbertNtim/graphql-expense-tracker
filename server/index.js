5197c383d64f7b01583d91b0e847086af063eda6
Herbert <herbertntim15@gmail.com>
2025-02-19 16:17:26 +0000

    added express in graphQL

--- server/index.js
+++ server/index.js
@@ -1,14 +1,37 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import cors from "cors";
+import http from "http";
 import mergedResolver from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
+const app = express();
+
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolver,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server);
+await server.start();
 
-console.log(`🚀  Server ready at: ${url}`);
+app.use(
+  "/",
+  express.json(),
+  cors(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  })
+)
+
+await new Promise((resolve) =>
+
+  httpServer.listen({ port: 4000 }, resolve),
+
+);
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
